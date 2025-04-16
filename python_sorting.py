220ea1a010311e0d1b90335243fa7da64eaea820
shaikusmangit <usmaan579@gmail.com>
2025-04-16 17:12:14 +0530

    created python sorting file with functions

---
+++ python_sorting.py
@@ -0,0 +1,26 @@
+#1. 🔁 Basic Bubble Sort#
+
+def bubble_sort_basic(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+##2. ⚡ Optimized Bubble Sort (with swap check)##
+##Stops early if no swaps are made in a pass — avoids unnecessary iterations.##
+
+
+def bubble_sort_optimized(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
